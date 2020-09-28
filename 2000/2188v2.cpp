cc334bb7d2354d856bca9523788fbf2a2391e700
ghdwlsrl3229 <35536038+ghdwlsrl3229@users.noreply.github.com>
2020-09-28 16:02:36 +0900

    Create 2188v2.cpp

---
+++ 2000/2188v2.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <unordered_map>
+#include <random>
+using namespace std;
+#define ll long long
+#define fr first
+#define sc second
+#define pll pair<ll, ll>
+#define all(v) v.begin(), v.end()
+
+const int MAX_V = 220;
+int N, M, A[MAX_V], B[MAX_V];
+vector<int> adj[MAX_V];
+bool visited[MAX_V];
+
+bool dfs(int a) {
+	visited[a] = 1;
+	for (int b : adj[a]) {
+		if (B[b] == -1 || !visited[B[b]] && dfs(B[b])) {
+			B[b] = a;
+			A[a] = b;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void solve() {
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		int a; cin >> a;
+		while (a--) {
+			int b; cin >> b;
+			adj[i].push_back(b);
+		}
+	}
+
+	int ans = 0;
+	memset(A, -1, sizeof(A));
+	memset(B, -1, sizeof(B));
+	for (int i = 1; i <= N; i++) {
+		if (A[i] == -1) {
+			memset(visited, 0, sizeof(visited));
+			if (dfs(i)) ans++;
+		}
+	}
+	cout << ans << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	if (0) {
+		int T; cin >> T;
+		while (T--) solve();
+	}
+	else {
+		solve();
+	}
+	return 0;
+}
